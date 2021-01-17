b3913a8eb2a8451fe927857009534180a5196d94
Tiziana Mancinelli <tiziana.mancinelli@gmail.com>
2021-01-17 22:37:48 +0100

    Search fixed for safari

--- js/search.js
+++ js/search.js
@@ -1,63 +1,57 @@
 const Search = {
   submit(e, form) {
     e.preventDefault();
-    new FormData(form);
+    this.formdata(new FormData(form));
     return false;
   },
 
-  formdata(event, form) {
+  formdata(formData) {
     let url = new URL(Biflow.baseurl + '/search', location);
 
-    if (!event.formData) {
-      location = url;
-      return;
+    if (!formData) {
+        location = url;
+        return;
     }
 
-    if (!event.formData.has("category")) {
-      event.formData.forEach((value, key) => url.searchParams.append(key, value));
-      location = url;
-      return;
-    }
-
-    switch (event.formData.get("category")) {
+    switch (formData.get("category")) {
       case 'work':
         url = new URL(Biflow.baseurl + '/works/', location);
-        url.searchParams.set("filter", event.formData.get("search"))
+        url.searchParams.set("filter", formData.get("search"))
         location = url;
         return;
 
       case 'manuscript':
         url = new URL(Biflow.baseurl + '/manuscripts/', location);
-        url.searchParams.set("filter", event.formData.get("search"))
+        url.searchParams.set("filter", formData.get("search"))
         location = url;
         return;
 
       case 'person':
         url = new URL(Biflow.baseurl + '/people/', location);
-        url.searchParams.set("filter", event.formData.get("search"))
+        url.searchParams.set("filter", formData.get("search"))
         location = url;
         return;
 
       case 'language':
         url = new URL(Biflow.baseurl + '/languages/', location);
-        url.searchParams.set("filter", event.formData.get("search"))
+        url.searchParams.set("filter", formData.get("search"))
         location = url;
         return;
 
       case 'genre':
         url = new URL(Biflow.baseurl + '/genres/', location);
-        url.searchParams.set("filter", event.formData.get("search"))
+        url.searchParams.set("filter", formData.get("search"))
         location = url;
         return;
 
       case 'library':
         url = new URL(Biflow.baseurl + '/libraries/', location);
-        url.searchParams.set("filter", event.formData.get("search"))
+        url.searchParams.set("filter", formData.get("search"))
         location = url;
         return;
 
       default:
-        event.formData.forEach((value, key) => url.searchParams.append(key, value));
+        formData.forEach((value, key) => url.searchParams.append(key, value));
         location = url;
         return;
     }
